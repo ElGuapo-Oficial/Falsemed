2aa91c92dc0a0c4d8cec6904d61cabaa66139607
Najash Marron <najashmarron@gmail.com>
2024-03-13 19:27:30 -0700

    vercel build fixes

--- src/app/components/categories-banner/CategoriesBanner.tsx
+++ src/app/components/categories-banner/CategoriesBanner.tsx
@@ -1,4 +1,5 @@
 import styles from "./CategoriesBanner.module.css";
+import Image from 'next/image';
 
 const CategoriesBanner = () => {
     return (
@@ -6,7 +7,7 @@ const CategoriesBanner = () => {
             <div className={styles["categories-banner-content"]}>
             <div className={styles["categories-banner-text"]}>
                 <p className={styles["greeny-text"]}>FOR CONSUMERS</p>
-                <p className={styles["boldy-text"]}>See if You're Qualified</p>
+                <p className={styles["boldy-text"]}>{"See if You're Qualified"}</p>
                 <p className={styles["blacky-text"]}>Qualified Customers can put their pre-tax HSA/FSA dollars to work</p>
                 <div className={styles.search}>
                     <div className={styles["inside-search"]}> 
@@ -15,7 +16,7 @@ const CategoriesBanner = () => {
                     </div>
                 </div>
                 <p className={styles["boldy-text"]}>Browse Categories</p>
-                <img src="Truemed_Logo_Full_White.png" alt="Truemed logo"/>
+                <Image src="Truemed_Logo_Full_White.png" alt="Truemed logo"/>
             </div>
             </div>
         </div>
