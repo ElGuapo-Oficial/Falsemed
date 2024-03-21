f9a262f2ae3678c95bc49c4e70d26d8320a3b634
Najash Marron <najashmarron@gmail.com>
2024-03-20 19:28:34 -0700

    Braintree build fixes

--- src/actions/braintree-actions.ts
+++ src/actions/braintree-actions.ts
@@ -2,11 +2,15 @@
 
 import braintree from 'braintree';
 
+if (!process.env.BRAINTREE_MERCHANT_ID || !process.env.BRAINTREE_PUBLIC_KEY || !process.env.BRAINTREE_PRIVATE_KEY) {
+    throw new Error("Braintree environment variables are not set.");
+}
+
 const gateway = new braintree.BraintreeGateway({
   environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY
+  merchantId: process.env.BRAINTREE_MERCHANT_ID as string,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY as string,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY as string,
 });
 
 export const generateClientToken = async (): Promise<string> => {
